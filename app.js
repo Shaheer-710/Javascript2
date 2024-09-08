d09e25445c5ec9443f71aa24b12877acf20106db
Shaheer-710 <shaheer7w@gmail.com>
2024-09-08 14:10:18 +0500

    commit these

---
+++ app.js
@@ -0,0 +1,86 @@
+// Chapter 1 (Alert)
+//  q1
+// alert(
+// "welcome to my page"
+// )
+// q2
+//  alert(
+//       "Error!Please enter a valid password "
+//  )
+// q3
+// alert(
+//     "welcome to js land .... \n happy coding"
+// )
+// q4
+// alert(
+//     "welcome to js land...."
+// )
+// alert(
+//     "Happy Coding! \n  "
+// )
+// q5
+// alert(
+//     "Hello i can run js through my web browser's control"
+// )
+// VARIABLES FOR STRINGS (ch#2)
+// q1
+// var username
+// q2
+// var myName="Muhaamad Shaheer"
+// q3
+// var message="Hello World"
+// alert(
+//     message
+// )
+// var biodata="jhon doe"
+// var age=15
+// var skills="Certified Mobile application development"
+// alert(
+//     biodata
+// )
+// alert(
+//     age
+// )
+// alert(
+//   skills
+// )
+// q5
+// var q5=
+//     "pizza \n pizz \n pi \n p"
+// alert(
+//     q5
+// )
+// q6
+// var email="shaherr7w@com"
+// alert(
+//     "My email adress is"+ email
+// )
+// q7
+// var book="A smarter way to learn JavaScript"
+// alert(
+//     "i am trying to learn javascript from the book"+ book
+// )
+// q8
+// var style="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(
+//     style
+// )
+// VARIABLES FOR NUMBERS (ch3)
+// var age=14
+// alert(
+//     age
+// )
+// var site
+// site = "Youhave visited this site 30 times"
+// alert(site)
+// var birthYear= "my birth year is 2010"
+// document.write(
+//     birthYear
+// ) 
+var visitorsName=" Jhon "
+
+var productName=" T-shirt(s)"
+var quantity
+quantity= "  5" 
+document.write( visitorsName+"ordered"+ quantity+ productName+"from xyzclothing.com" 
+)
